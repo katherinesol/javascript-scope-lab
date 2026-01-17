dc7c76a7b142071a9c09ddf3674160a862fd6bc6
“katherinesol” <katherine.sol@pm.me>
2026-01-17 13:43:41 -0500

    Global Scope Variables

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Declare global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Create function-scoped variable
+function addBurger() {
+    const newBurger = 'Flatburger';
+    burgers.push(newBurger);
+}
+
+// Create block-scoped variable
+if(true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+}
+
+// Change featured drink name
+function changeFeaturedDrink() {
+    featuredDrink = 'The JavaShake';
+}
